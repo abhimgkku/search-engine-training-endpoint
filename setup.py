fe0539cd855db5fa78bc63e0223141caa6acc274
abhinav kumar <abhinav.mg.k@gmail.com>
2024-12-06 18:12:58 +0530

    training_endpoint_created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Abhinav Kumar",
+    author_email="abhinav.mg.k@gmail.com",
+    packages=find_packages()
+)
